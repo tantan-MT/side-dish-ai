0b6c1f9f7ab40b999770fded767ebd1ae97841b5
tantan-MT <you@example.com>
2026-05-14 22:10:01 +0900

    Configure OpenAI only when API key is present

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,3 +1,5 @@
-OpenAI.configure do |config|
-  config.access_token = ENV["OPENAI_API_KEY"]
+if ENV["OPENAI_API_KEY"].present?
+  OpenAI.configure do |config|
+    config.access_token = ENV["OPENAI_API_KEY"]
+  end
 end
