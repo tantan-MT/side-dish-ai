4f60467cc82fc3649584ff2f1b4a0b4dcad14406
tantan-MT <you@example.com>
2026-06-24 23:36:48 +0900

    Add recipe ingredient association

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ApplicationRecord
   belongs_to :ingredient_category
+
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :recipes, through: :recipe_ingredients
 end
